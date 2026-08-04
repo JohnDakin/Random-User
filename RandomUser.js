c9f0e5a42efdef6c85bc6b390769444969867791
John Dakin <jdakin749@gmail.com>
2026-08-04 14:59:04 +0300

    Fetching Random User

---
+++ RandomUser.js
@@ -0,0 +1,33 @@
+const url = "https://randomuser.me/api/";
+
+
+async function getRandomUser(url){
+  try{
+    let response = await fetch(url);
+    // console.log(response);
+
+    if(!response.ok){
+      throw new Error("Unable to fetch resource, Response: ", response.status)
+    }
+
+    let data = await response.json()
+    // console.log(`${data.results[0].name.first} ${data.results[0].name.last} is ${data.results[0].gender} in gender`);
+    // console.log(data.results[0].picture.large)
+
+    let randomUserImage = data.results[0].picture.large;
+
+    let htmlImage = document.getElementById("randomUserImage");
+    let htmlP = document.getElementById("userDetail");
+
+    htmlImage.src =  randomUserImage;
+    htmlImage.style.display = "block";
+    htmlP.innerText  = `${data.results[0].name.first} ${data.results[0].name.last} is ${data.results[0].gender} in gender`;
+
+
+  }catch(error){
+    console.log("Error",error.message);
+  }
+  
+}
+
+getRandomUser(url);
