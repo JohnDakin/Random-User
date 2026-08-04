fa04811f1e4ed8ce10bfe8750cbf662c381e366e
John Dakin <jdakin749@gmail.com>
2026-08-04 17:51:01 +0300

    Updated username object

--- RandomUser.js
+++ RandomUser.js
@@ -15,13 +15,13 @@ async function getRandomUser(url){
     // console.log(data.results[0].picture.large)
 
     let randomUserImage = data.results[0].picture.large;
-
+    let randomUserNameObject = data.results[0].name; 
     let htmlImage = document.getElementById("randomUserImage");
     let htmlP = document.getElementById("userDetail");
 
     htmlImage.src =  randomUserImage;
     htmlImage.style.display = "block";
-    htmlP.innerText  = `${data.results[0].name.first} ${data.results[0].name.last} is ${data.results[0].gender} in gender`;
+    htmlP.innerText  = `${randomUserNameObject.first} ${randomUserNameObject.last} is ${data.results[0].gender} in gender`;
 
 
   }catch(error){
