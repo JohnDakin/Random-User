e11898adfc80c792add6a5ed9067aaaa8c8c9f0a
John Dakin <jdakin749@gmail.com>
2026-08-04 18:41:27 +0300

    displayed user login details on the page

--- scripts.js
+++ scripts.js
@@ -13,15 +13,29 @@ async function getRandomUser(url){
     let data = await response.json()
     // console.log(`${data.results[0].name.first} ${data.results[0].name.last} is ${data.results[0].gender} in gender`);
     // console.log(data.results[0].picture.large)
+    console.log(data);
 
     let randomUserImage = data.results[0].picture.large;
     let randomUserNameObject = data.results[0].name; 
     let htmlImage = document.getElementById("randomUserImage");
-    let htmlP = document.getElementById("userDetail");
+    let htmlGender = document.getElementById("userGender");
+    let htmlUsername = document.getElementById("userName");
+    let loginDetail = document.getElementById("loginDetail");
+    let loginDetailObject = data.results[0].login;
+    console.log(loginDetailObject);
+
+    loginDetail.innerHTML = "";
+    
+    for(let detail in loginDetailObject){
+      loginDetail.innerHTML += `<p>${detail}: ${loginDetailObject[detail]}</p>`
+    }
+
 
     htmlImage.src =  randomUserImage;
+    htmlUsername.innerText = `Username: ${randomUserNameObject.title}. ${randomUserNameObject.first} ${randomUserNameObject.last}`;
+
     htmlImage.style.display = "block";
-    htmlP.innerText  = `${randomUserNameObject.first} ${randomUserNameObject.last} is ${data.results[0].gender} in gender`;
+    htmlGender.innerText  = `Gender: ${data.results[0].gender.charAt(0).toUpperCase() + data.results[0].gender.slice(1)}`;
 
 
   }catch(error){
